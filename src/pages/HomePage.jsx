5d5c9c0aa254691a25fcd4a3df38cb97810d96cf
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-06-04 16:24:16 -0500

    V 0.0.6
    Added icons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,13 @@
+import { IoInformationCircleSharp } from "react-icons/io5";
+import { PiEngineFill } from "react-icons/pi";
+import { GiSpring } from "react-icons/gi";
+import { FaGears } from "react-icons/fa6";
+import { GiCarWheel } from "react-icons/gi";
+import { GiGearStickPattern } from "react-icons/gi";
+import { GiSteeringWheel } from "react-icons/gi";
+import { MdAir } from "react-icons/md";
+import { MdOutlineSecurity } from "react-icons/md";
+import { IoCarSport } from "react-icons/io5";
 
 import Buttom from '../components/Buttom'
 import Profile from '../components/Profile'
@@ -11,7 +21,60 @@ const HomePage = () => {
   return (
     <>
       <div className='container'>
-        <div className='container_lateral_menu'></div>
+        <div className='container_lateral_menu'>
+          {/* Header icons */}
+          <div className='container_icon_header'>
+            <div className='icon_header_grid'>
+              <div className='iconsBack'></div>
+              <div className='iconFront'></div>
+              <div className='iconLateral'></div>
+            </div>
+          </div>
+
+          {/* Icons content */}
+          <div className='container_content'>
+            <div className="container_items">
+              <p className="content_icon"><IoInformationCircleSharp /></p>
+              <span className="content_description">Informaciones generales</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><PiEngineFill /></p>
+              <span className="content_description">Motor</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><GiSpring /></p>
+              <span className="content_description">Suspensiones</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><FaGears /></p>
+              <span className="content_description">Sistema de transmisión/Ejes</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><GiCarWheel /></p>
+              <span className="content_description">Frenos</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><GiGearStickPattern /></p>
+              <span className="content_description">Cambio/Cambio con diferencial</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><GiSteeringWheel /></p>
+              <span className="content_description">Dirección</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><MdAir /></p>
+              <span className="content_description">Calentador, ventilador y aire acondicionado</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><MdOutlineSecurity /></p>
+              <span className="content_description">Dispositivos de retensión</span>
+            </div>
+            <div className="container_items">
+              <p className="content_icon"><IoCarSport /></p>
+              <span className="content_description">Carroceria y accesorios</span>
+            </div>
+          </div>
+        </div>
         <div className='container_header'>
           <div className='searchBox'>
             <SearchEngine/>
